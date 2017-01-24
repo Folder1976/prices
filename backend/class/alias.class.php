3b061bf61a5bb882da932f2c9218160cd90278ef
Folder <folder.list@gmail.com>
2017-01-24 15:13:52 +0200

    alias generator

--- backend/class/alias.class.php
+++ backend/class/alias.class.php
@@ -28,6 +28,7 @@ class Alias
 		
 		
 		$alias = trim($alias,'-');
+	
 		
 		
 		$sql = 'SELECT sku, name, code
@@ -36,7 +37,12 @@ class Alias
 					WHERE P.product_id="'.$product_id.'" LIMIT 0,1;';
 		$r = $this->db->query($sql) or die($sql);
 		if($r->num_rows > 0){
+			
 			$row = $r->fetch_assoc();
+			
+			$row['code'] = urldecode($row['code']);
+			$row['sku'] = urldecode($row['sku']);
+			
 			if($row['code'] != ''){
 				$alias .= '/'.strtolower($row['code']);
 			}elseif($row['sku'] != ''){
@@ -45,6 +51,20 @@ class Alias
 				$alias .= '/'.strtolower($this->translitArtkl($row['name'].'-'.$product_id));
 			}
 		}
+		
+		
+		
+		
+		$alias = trim($alias);
+		$alias = str_replace('"','-', $alias);
+		$alias = str_replace('%20','-', $alias);
+		$alias = str_replace('®','-', $alias);
+		$alias = str_replace('--','-', $alias);
+		$alias = str_replace('--','-', $alias);
+		$alias = str_replace('--','-', $alias);
+		$alias = str_replace('--','-', $alias);
+		
+		echo $alias.'<br>';
 		
 		return $alias;
 		
@@ -263,8 +283,8 @@ class Alias
 	}
 	
 	private function translitArtkl($str) {
-		$rus = array('и','і','є','Є','ї','\"','\'','.',' ','А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
-	    $lat = array('u','i','e','E','i','','','','-','A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
+		$rus = array('и','<','>','/','%20','(',')',':','.',',','і','є','Є','ї','\"','\'','.',' ','А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+	    $lat = array('u','','','','-','','','','','','i','e','E','i','','','','-','A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
 	return str_replace($rus, $lat, $str);
   }
 	
